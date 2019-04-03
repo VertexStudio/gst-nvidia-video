74f48ae0aca88dcd5adb01aff35bf6194297617a
Raúl Pleitez <racsoraul@gmail.com>
2019-04-03 17:22:38 -0600

    alsa audio enabled.

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,3 @@
- docker run --runtime=nvidia -ti -v $(pwd):/home/gst --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE --name gst gst-nvidia-video
+ docker run --runtime=nvidia -ti -v $(pwd):/home/gst --network=host -v /dev/snd:/dev/snd \
+    --privileged -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --cap-add=SYS_PTRACE --name gst gst-nvidia-video
